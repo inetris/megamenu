6e49fa273dbab972bc34256518a6ce09cce48c29
Алексей Ковригин <83507071+alekseikovrigin@users.noreply.github.com>
2022-11-30 23:43:06 +0300

    Add files via upload

---
+++ lang/ru/install/index.php
@@ -0,0 +1,8 @@
+<?php
+$MESS["MEGAMENU_NAME"] = "Мегаменю";
+$MESS["MEGAMENU_DESCRIPTION"] = "Модуль позволяет создавать произвольные мегаменю";
+$MESS["MEGAMENU_PARTNER_NAME"] = "Inetris";
+$MESS["MEGAMENU_FILE_NOT_FOUND"] = "Не найден файл #FILE#";
+$MESS["MEGAMENU_IBLOCK_NAME"] = "Мегаменю раздела Каталог";
+$MESS["MEGAMENU_PROP_NAME_FILE"] = "Файл";
+$MESS["MEGAMENU_PROP_NAME_MAIL"] = "Почта";
